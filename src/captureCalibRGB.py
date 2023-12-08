cf5412058a86ba2b31334015425a6613b685b054
Yumekawa <82374688+Yumekawa-chan@users.noreply.github.com>
2023-12-08 19:34:16 +0900

    Update captureCalibRGB.py

--- src/captureCalibRGB.py
+++ src/captureCalibRGB.py
@@ -1,11 +1,10 @@
 import socket
 import datetime
 import cv2
-import threading
 import io
 
 def capture_image():
-    cap = cv2.VideoCapture(0) 
+    cap = cv2.VideoCapture(0)
     ret, frame = cap.read()
     if not ret:
         print("Failed to capture image")
@@ -19,27 +18,20 @@ def capture_image():
 
     return mem_buffer.getvalue()
 
-def handle_client(client, jetson_id):
-    while True:
-        data = client.recv(1024)
-        if data == b'capture':
-            image_data = capture_image()
-            if image_data:
-                timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-                filename = f"{timestamp}_{jetson_id}.png"
-                client.sendall(filename.encode() + b'\n' + image_data)
+def connect_and_handle_server(jetson_id):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+        client_socket.connect(('192.168.10.117', 65432))
+        print(f"Connected to server at 192.168.10.117:65432")
 
-def accept_clients():
-    while True:
-        client, addr = server.accept()
-        jetson_id = len(clients) + 1 
-        clients.append(client)
-        print(f'Connected by {addr}')
-        threading.Thread(target=handle_client, args=(client, jetson_id), daemon=True).start()
+        while True:
+            data = client_socket.recv(1024)
+            if data == b'capture':
+                image_data = capture_image()
+                if image_data:
+                    timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+                    filename = f"{timestamp}_{jetson_id}.png"
+                    client_socket.sendall(filename.encode() + b'\n')
+                    client_socket.sendall(image_data)
 
-clients = []
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(('0.0.0.0', 65432))
-server.listen()
-
-accept_clients()
+jetson_id = 1
+connect_and_handle_server(jetson_id)

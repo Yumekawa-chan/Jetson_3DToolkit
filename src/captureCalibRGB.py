f6266012f93ad504f8c8b52c909cc64a2ffcf1de
Yumekawa <82374688+Yumekawa-chan@users.noreply.github.com>
2023-12-08 18:41:45 +0900

    Update captureCalibRGB.py

--- src/captureCalibRGB.py
+++ src/captureCalibRGB.py
@@ -1,24 +1,49 @@
 import socket
+import datetime
+import cv2
 import threading
+import io
 
+def capture_image():
+    # カメラの初期化
+    cap = cv2.VideoCapture(0)  # 0はデフォルトのカメラを指します
+    ret, frame = cap.read()
+    if not ret:
+        print("Failed to capture image")
+        return None
 
-def wait_for_key():
-    input("Press Enter to send capture command...")
-    for client in clients:
-        client.sendall(b'capture')
+    # メモリバッファに画像を保存
+    mem_buffer = io.BytesIO()
+    is_success, buffer = cv2.imencode(".png", frame)
+    mem_buffer.write(buffer)
 
+    # カメラのリリース
+    cap.release()
+
+    # バッファからバイトデータを取得
+    return mem_buffer.getvalue()
+
+def handle_client(client, jetson_id):
+    while True:
+        data = client.recv(1024)
+        if data == b'capture':
+            image_data = capture_image()
+            if image_data:
+                timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+                filename = f"{timestamp}_{jetson_id}.png"
+                client.sendall(filename.encode() + b'\n' + image_data)
 
 def accept_clients():
     while True:
         client, addr = server.accept()
+        jetson_id = len(clients) + 1  # JetsonデバイスのID
         clients.append(client)
         print(f'Connected by {addr}')
-
+        threading.Thread(target=handle_client, args=(client, jetson_id), daemon=True).start()
 
 clients = []
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(('0.0.0.0', 65432))
 server.listen()
 
-threading.Thread(target=accept_clients).start()
-wait_for_key()
+accept_clients()

b70de62940869d085179b6616207ed4759440028
Yumekawa <82374688+Yumekawa-chan@users.noreply.github.com>
2023-12-09 16:18:36 +0900

    Update captureRGBD.py

--- src/captureRGBD.py
+++ src/captureRGBD.py
@@ -1,70 +1,67 @@
-import os
-import cv2
-import numpy as np
+import socket
+import datetime
 import pyrealsense2 as rs
-from datetime import datetime
+import numpy as np
+import cv2
+import io
 
+def capture_image():
+    # Configure depth and color streams
+    pipeline = rs.pipeline()
+    config = rs.config()
+    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 
-class RealSenseCapture:
-    DEPTH_MIN = 150
-    DEPTH_MAX = 15000
+    # Start the camera pipeline
+    pipeline.start(config)
 
-    def __init__(self):
-        self.pipeline = rs.pipeline()
-        self.configure_pipeline()
-        self.pc = rs.pointcloud()
-        self.initialize_directories()
-
-    def configure_pipeline(self):
-        config = rs.config()
-        config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-        config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
-        self.pipeline.start(config)
-
-    def initialize_directories(self):
-        base_dir = os.path.dirname(os.path.abspath(__file__))
-        folder_name = datetime.now().strftime("%Y%m%d")
-        self.pointclouds_dir = os.path.join(
-            base_dir, '..', 'data', 'pointclouds', folder_name)
-        self.depths_dir = os.path.join(
-            base_dir, '..', 'data', 'depths', folder_name)
-        self.colors_dir = os.path.join(
-            base_dir, '..', 'data', 'colors', folder_name)
-        os.makedirs(self.pointclouds_dir, exist_ok=True)
-
-    def process_frame(self):
-        frames = self.pipeline.wait_for_frames()
+    try:
+        frames = pipeline.wait_for_frames()
         depth_frame = frames.get_depth_frame()
         color_frame = frames.get_color_frame()
-        depth_image = self.process_depth_frame(depth_frame)
-        return depth_frame, color_frame, depth_image
 
-    def process_depth_frame(self, depth_frame):
+        if not depth_frame or not color_frame:
+            print("Failed to capture images")
+            return None, None
+
+        # Convert images to numpy arrays
         depth_image = np.asanyarray(depth_frame.get_data())
-        depth_image[(depth_image < self.DEPTH_MIN) |
-                    (depth_image > self.DEPTH_MAX)] = 0
-        depth_colormap = cv2.normalize(
-            depth_image, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
-        depth_colormap = cv2.medianBlur(depth_colormap, 5)
-        return depth_colormap
+        color_image = np.asanyarray(color_frame.get_data())
 
-    def save_pointcloud(self, depth_frame, color_frame):
-        timestamp = datetime.now().strftime("%H%M%S")
-        self.pc.map_to(color_frame)
-        pointcloud = self.pc.calculate(depth_frame)
-        ply_file_path = os.path.join(self.pointclouds_dir, f'{timestamp}.ply')
-        pointcloud.export_to_ply(ply_file_path, color_frame)
-        print(f'Pointcloud saved at {ply_file_path}')
+        # Apply colormap on depth image
+        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
 
-    def stop(self):
-        self.pipeline.stop()
+        # Encode images
+        mem_buffer_color = io.BytesIO()
+        mem_buffer_depth = io.BytesIO()
+        is_success_color, buffer_color = cv2.imencode(".png", color_image)
+        is_success_depth, buffer_depth = cv2.imencode(".png", depth_colormap)
+        mem_buffer_color.write(buffer_color)
+        mem_buffer_depth.write(buffer_depth)
 
-
-if __name__ == "__main__":
-    capture = RealSenseCapture()
-    try:
-        while True:
-            depth_frame, color_frame, depth_image = capture.process_frame()
-            capture.save_pointcloud(depth_frame, color_frame)
+        return mem_buffer_color.getvalue(), mem_buffer_depth.getvalue()
     finally:
-        capture.stop()
+        pipeline.stop()
+
+def connect_and_handle_server(jetson_id):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+        client_socket.connect(('192.168.10.117', 65432))
+        print(f"Connected to server at 192.168.10.117:65432")
+
+        while True:
+            data = client_socket.recv(1024)
+            if data == b'capture':
+                color_image_data, depth_image_data = capture_image()
+                if color_image_data and depth_image_data:
+                    timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+                    color_filename = f"{timestamp}_{jetson_id}_color.png"
+                    depth_filename = f"{timestamp}_{jetson_id}_depth.png"
+                    client_socket.sendall(color_filename.encode() + b'\n')
+                    client_socket.sendall(color_image_data)
+                    client_socket.sendall(b'ENDOFIMAGE')
+                    client_socket.sendall(depth_filename.encode() + b'\n')
+                    client_socket.sendall(depth_image_data)
+                    client_socket.sendall(b'ENDOFIMAGE')
+
+jetson_id = 1
+connect_and_handle_server(jetson_id)

116ace653b1edeb8ccbc170d9c4fe078cd3aa3b9
Yumekawa <82374688+Yumekawa-chan@users.noreply.github.com>
2023-12-09 16:39:47 +0900

    Update captureRGBD.py

--- src/captureRGBD.py
+++ src/captureRGBD.py
@@ -1,29 +1,49 @@
 import socket
 import datetime
+import pyrealsense2 as rs
+import numpy as np
 import cv2
 import io
 
 def capture_image():
-    cap = cv2.VideoCapture(2)
-    ret, frame = cap.read()
-    cap.release()
-    if not ret:
-        print("Failed to capture image")
-        return None, None
+    # Configure depth and color streams
+    pipeline = rs.pipeline()
+    config = rs.config()
+    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 
-    color_image = frame
-    depth_image = frame 
+    # Start streaming
+    pipeline.start(config)
 
-    mem_buffer_color = io.BytesIO()
-    is_success_color, buffer_color = cv2.imencode(".png", color_image)
-    mem_buffer_color.write(buffer_color)
+    try:
+        # Wait for a coherent pair of frames: depth and color
+        frames = pipeline.wait_for_frames()
+        depth_frame = frames.get_depth_frame()
+        color_frame = frames.get_color_frame()
 
+        if not depth_frame or not color_frame:
+            print("Could not acquire depth or color frames.")
+            return None, None
 
-    mem_buffer_depth = io.BytesIO()
-    is_success_depth, buffer_depth = cv2.imencode(".png", depth_image)
-    mem_buffer_depth.write(buffer_depth)
+        # Convert images to numpy arrays
+        depth_image = np.asanyarray(depth_frame.get_data())
+        color_image = np.asanyarray(color_frame.get_data())
 
-    return mem_buffer_color.getvalue(), mem_buffer_depth.getvalue()
+        # Convert depth image to 8-bit for visualization
+        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+
+        # Encode the frames as jpeg to send over the network
+        is_success_color, buffer_color = cv2.imencode(".png", color_image)
+        is_success_depth, buffer_depth = cv2.imencode(".png", depth_colormap)
+
+        if not is_success_color or not is_success_depth:
+            print("Could not encode images.")
+            return None, None
+
+        return buffer_color.tobytes(), buffer_depth.tobytes()
+    finally:
+        # Stop streaming
+        pipeline.stop()
 
 def connect_and_handle_server(jetson_id):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:

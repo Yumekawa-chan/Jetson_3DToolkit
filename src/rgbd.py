c0d7bb41c470601804a3cf38defbf0e2291e2fb9
Yumekawa <82374688+Yumekawa-chan@users.noreply.github.com>
2023-12-09 14:57:27 +0900

    Update rgbd.py

--- src/rgbd.py
+++ src/rgbd.py
@@ -1,10 +1,11 @@
 import socket
-import datetime
+import pyrealsense2 as rs
 import numpy as np
 import cv2
-import pyrealsense2 as rs
+import io
+import datetime
 
-def capture_images():
+def capture_image_and_params():
     pipeline = rs.pipeline()
     config = rs.config()
     config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
@@ -17,48 +18,47 @@ def capture_images():
         color_frame = frames.get_color_frame()
 
         if not depth_frame or not color_frame:
-            return None, None
+            return None, None, None
 
         depth_image = np.asanyarray(depth_frame.get_data())
         color_image = np.asanyarray(color_frame.get_data())
 
-        return depth_image, color_image
+        depth_intrinsics = depth_frame.profile.as_video_stream_profile().intrinsics
+        params = np.array([depth_intrinsics.width, depth_intrinsics.height, depth_intrinsics.fx, depth_intrinsics.fy, depth_intrinsics.ppx, depth_intrinsics.ppy], dtype=np.float32)
+
+        return depth_image, color_image, params
     finally:
         pipeline.stop()
 
-def connect_and_handle_server(jetson_id):
+def connect_and_handle_server(jetson_id, server_ip, server_port):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
-        client_socket.connect(('192.168.10.117', 65432))
-        print(f"Connected to server at 192.168.10.117:65432")
-
-        # Send camera parameters
-        pipeline = rs.pipeline()
-        config = rs.config()
-        pipeline.start(config)
-        profile = pipeline.get_active_profile()
-        depth_profile = rs.video_stream_profile(profile.get_stream(rs.stream.depth))
-        intrinsics = depth_profile.get_intrinsics()
-        params_data = np.array([intrinsics.width, intrinsics.height, intrinsics.fx, intrinsics.fy, intrinsics.ppx, intrinsics.ppy], dtype=np.float32).tobytes()
-        params_size_info = f"{len(params_data)}\n".encode()
-        client_socket.sendall(params_size_info + params_data)
-        pipeline.stop()
+        client_socket.connect((server_ip, server_port))
+        print(f"Connected to server at {server_ip}:{server_port}")
 
         while True:
             data = client_socket.recv(1024)
             if data == b'capture':
-                depth_image, color_image = capture_images()
+                depth_image, color_image, params = capture_image_and_params()
                 if depth_image is not None and color_image is not None:
                     # Send depth image
-                    depth_filename = f"{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}_{jetson_id}_depth.png"
+                    timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+                    depth_filename = f"{timestamp}_{jetson_id}_depth.png"
                     client_socket.sendall(depth_filename.encode() + b'\n')
-                    is_success, buffer = cv2.imencode(".png", depth_image)
+                    _, buffer = cv2.imencode(".png", depth_image)
                     client_socket.sendall(buffer.tobytes() + b'ENDOFIMAGE')
 
                     # Send color image
-                    color_filename = f"{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}_{jetson_id}_color.png"
+                    color_filename = f"{timestamp}_{jetson_id}_color.png"
                     client_socket.sendall(color_filename.encode() + b'\n')
-                    is_success, buffer = cv2.imencode(".png", color_image)
+                    _, buffer = cv2.imencode(".png", color_image)
                     client_socket.sendall(buffer.tobytes() + b'ENDOFIMAGE')
 
+                    # Send parameters
+                    param_filename = f"{timestamp}_{jetson_id}_params.npy"
+                    client_socket.sendall(param_filename.encode() + b'\n')
+                    client_socket.sendall(params.tobytes() + b'ENDOFPARAMS')
+
 jetson_id = 1
-connect_and_handle_server(jetson_id)
+server_ip = '192.168.10.117'  # Server IP address
+server_port = 65432  # Server port
+connect_and_handle_server(jetson_id, server_ip, server_port)

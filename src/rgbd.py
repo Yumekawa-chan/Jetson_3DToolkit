c04130e9e676f7127fcb21e600048975941d617e
Yumekawa <82374688+Yumekawa-chan@users.noreply.github.com>
2023-12-09 15:56:30 +0900

    Update rgbd.py

--- src/rgbd.py
+++ src/rgbd.py
@@ -1,15 +1,17 @@
 import socket
-import pyrealsense2 as rs
-import numpy as np
+import datetime
 import cv2
 import io
-import datetime
+import pyrealsense2 as rs
+import numpy as np
 
-def capture_image_and_params():
+def capture_image():
+    # RealSenseカメラの設定
     pipeline = rs.pipeline()
     config = rs.config()
     config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
     config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+
     pipeline.start(config)
 
     try:
@@ -18,47 +20,52 @@ def capture_image_and_params():
         color_frame = frames.get_color_frame()
 
         if not depth_frame or not color_frame:
-            return None, None, None
+            print("Could not acquire depth or color frames.")
+            return None, None
 
+        # Depth画像の変換
         depth_image = np.asanyarray(depth_frame.get_data())
+        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+
+        # Color画像の変換
         color_image = np.asanyarray(color_frame.get_data())
 
-        depth_intrinsics = depth_frame.profile.as_video_stream_profile().intrinsics
-        params = np.array([depth_intrinsics.width, depth_intrinsics.height, depth_intrinsics.fx, depth_intrinsics.fy, depth_intrinsics.ppx, depth_intrinsics.ppy], dtype=np.float32)
+        depth_mem_buffer = io.BytesIO()
+        color_mem_buffer = io.BytesIO()
 
-        return depth_image, color_image, params
+        is_success, depth_buffer = cv2.imencode('.png', depth_colormap)
+        is_success, color_buffer = cv2.imencode('.png', color_image)
+
+        depth_mem_buffer.write(depth_buffer)
+        color_mem_buffer.write(color_buffer)
+
+        return depth_mem_buffer.getvalue(), color_mem_buffer.getvalue()
     finally:
         pipeline.stop()
 
-def connect_and_handle_server(jetson_id, server_ip, server_port):
+def connect_and_handle_server(jetson_id):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
-        client_socket.connect((server_ip, server_port))
-        print(f"Connected to server at {server_ip}:{server_port}")
+        client_socket.connect(('192.168.10.117', 65432))
+        print(f"Connected to server at 192.168.10.117:65432")
 
         while True:
             data = client_socket.recv(1024)
             if data == b'capture':
-                depth_image, color_image, params = capture_image_and_params()
-                if depth_image is not None and color_image is not None:
-                    # Send depth image
+                depth_data, color_data = capture_image()
+                if depth_data and color_data:
                     timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
                     depth_filename = f"{timestamp}_{jetson_id}_depth.png"
-                    client_socket.sendall(depth_filename.encode() + b'\n')
-                    _, buffer = cv2.imencode(".png", depth_image)
-                    client_socket.sendall(buffer.tobytes() + b'ENDOFIMAGE')
-
-                    # Send color image
                     color_filename = f"{timestamp}_{jetson_id}_color.png"
-                    client_socket.sendall(color_filename.encode() + b'\n')
-                    _, buffer = cv2.imencode(".png", color_image)
-                    client_socket.sendall(buffer.tobytes() + b'ENDOFIMAGE')
 
-                    # Send parameters
-                    param_filename = f"{timestamp}_{jetson_id}_params.npy"
-                    client_socket.sendall(param_filename.encode() + b'\n')
-                    client_socket.sendall(params.tobytes() + b'ENDOFPARAMS')
+                    # Depth画像を送信
+                    client_socket.sendall(depth_filename.encode() + b'\n')
+                    client_socket.sendall(depth_data)
+                    client_socket.sendall(b'ENDOFIMAGE')
+
+                    # Color画像を送信
+                    client_socket.sendall(color_filename.encode() + b'\n')
+                    client_socket.sendall(color_data)
+                    client_socket.sendall(b'ENDOFIMAGE')
 
 jetson_id = 1
-server_ip = '192.168.10.117'  # Server IP address
-server_port = 65432  # Server port
-connect_and_handle_server(jetson_id, server_ip, server_port)
+connect_and_handle_server(jetson_id)
